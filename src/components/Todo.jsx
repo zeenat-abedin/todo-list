7b61c1845ef852e6e2c70d84c1c1d185d6206a1c
zeenat <zeenu.23@gmail.com>
2024-03-27 17:35:16 +0530

    modify todo-container styles, add Task component to display tasks

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import { useState } from 'react'
 import './Todo.css'
 
+function Task({ task, onDelete }) {
+    return <div className='task' >
+        {task.title} 
+    </div>
+}
+
+
 function Todo() {
+    const [tasks, setTasks] = useState([
+        {
+            title: "Buying a pizza",
+            completed: false
+        },
+        {
+            title: "Do your workout",
+            completed: true
+        },
+        {
+            title: "Hangout with friends",
+            completed: true
+        }
+    ])
+
   return (
     <div  className="todo-container">
-      <h1>To Do List</h1>
-      
+        <div className="header">TODO ITEMS</div> 
+          <div className="tasks">
+              {tasks.map((task) => (
+                  <Task key={task.title} task={task} tasks={tasks} />
+              ))}
+        </div>          
     </div>
   )
 }
