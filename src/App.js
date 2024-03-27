f01f18a6f2be912a0cd0a0f06cb499890e5fe316
zeenat <zeenu.23@gmail.com>
2024-03-27 16:47:09 +0530

    "Import and add Todo component to App"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Todo from "./components/Todo";
 
 
 function App() {
   return (
     <div>
-      
+      <Todo/>
     </div>
   );
 }
---
+++ src/components/Todo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Todo.css'
+
+function Todo() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Todo
