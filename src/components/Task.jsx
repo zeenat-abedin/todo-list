c20bcb73d08345efd41d041ac2fee031a4778b57
zeenat <zeenu.23@gmail.com>
2024-03-28 00:22:58 +0530

    "Add a 'Complete' button for each task in the Task component"

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,6 +3,7 @@ function Task({ task, index, completeTask }) {
     return (
     <div className='task' style={{ textDecoration: task.completed ? "line-through" : "" }}>
         {task.title} 
+        <button onClick={() => completeTask(index)}>Complete</button>
     </div>
     )
 }
